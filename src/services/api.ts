6c7c3fb0b31461b21304e97fab556871c1a83a6a
MarcosEdington <115179086+MarcosEdington@users.noreply.github.com>
2026-02-03 17:06:53 -0300

    configuracao da api no render

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'https://localhost:7157/api', // IMPORTANTE: Veja no seu Back-end qual é a porta (ex: 7121)
-  baseURL: 'https://api-gestao-clientes-2cd2.onrender.com', // RENDER
+  baseURL: 'https://api-gestao-clientes-2cd2.onrender.com/api', // RENDER
 });
 
 export default api;
