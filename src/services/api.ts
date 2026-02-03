f8a49090e1f652d49ef5c5619201f3ed24360332
MarcosEdington <115179086+MarcosEdington@users.noreply.github.com>
2026-02-03 16:14:31 -0300

    alteracao da api para o render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7157/api', // IMPORTANTE: Veja no seu Back-end qual é a porta (ex: 7121)
+  //baseURL: 'https://localhost:7157/api', // IMPORTANTE: Veja no seu Back-end qual é a porta (ex: 7121)
+  baseURL: 'https://api-gestao-clientes-2cd2.onrender.com', // RENDER
 });
 
 export default api;
